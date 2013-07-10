8c5d83e03e020b011dbbb401bd15bc5029408753
Fernando Carvajal <fcarvajal.es@gmail.com>
2013-07-10 18:37:05 +0000

    Corrected some errors in the original versions of grader.js and checks.json

--- grader.js
+++ grader.js
@@ -67,7 +67,7 @@ if (require.main == module) {
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
 	.parse(process.argv);
-    var checkJson = chekcHtmlFile(program.file, program.checks);
+    var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
