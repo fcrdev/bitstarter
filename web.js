ff31eaeb9c34c502d712ae1ae31ebdb761804a9d
Fernando Carvajal <fcarvajal.es@gmail.com>
2013-07-21 23:10:35 +0000

    Modified web.js so changes are refreshed inmediatedly without restarting the server

--- web.js
+++ web.js
@@ -3,13 +3,18 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var text = fs.readFileSync('index.html').toString();
+
+/* In order to test hw4 without restarting the server each time there is a change in index.html, it is the
+best to move the read of the file inside the apt.get callback function */
+   
+//var text = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
+  var text = fs.readFileSync('index.html').toString();
   response.send(text);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
